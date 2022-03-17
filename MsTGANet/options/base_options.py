ec8c97a4142429d356467fcb9837e7be5b1d4669
Julia-Brixey <juliabrixey23@gmail.com>
2022-03-17 10:21:24 -0500

    Unit testing progress

--- MsTGANet/options/base_options.py
+++ MsTGANet/options/base_options.py
@@ -9,14 +9,14 @@ class BaseOptions:
 	number_of_classes: int
 
 	learning_rate: float = 0.001
-	batch_size: int = 1
+	batch_size: int = 10
 
 	number_of_epochs: int = 200
 	epoch_between_decay: int = 50
 	decay_rate: float = 0.5
 
-	dataloader_threads: int = 70
-	device: str = "cuda"
+	dataloader_threads: int = 1
+	device: str = "cpu"
 	verbose: bool = True
 
 	background_label: int = 0
